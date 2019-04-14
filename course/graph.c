7c92a5fdb7bb2a998d436a08e90f7224e6a7031d
virpara <virpararahul@gmail.com>
2019-04-14 14:49:11 +0530

    added comments

--- course/graph.c
+++ course/graph.c
@@ -7,7 +7,7 @@
 struct edge_node {
     int v; // node value
     int weight; // edge weight e.g. distance, etc.
-    struct edge_node * next; // next node
+    struct edge_node * next; // next node to have linked list of nodes
 };
 typedef struct edge_node edge_node;
 
@@ -16,7 +16,7 @@ struct graph {
     // same as edge_node * edges[100], but declare dynamic size of array
     edge_node * * edges; // array of edge nodes in the graph
     int nb_nodes; // number of nodes in the graph
-    int ng_edges; // number of edges in the graph
+    int nb_edges; // number of edges in the graph
     bool is_directed; // whether graph is directed or not
 };
 typedef struct graph graph;
@@ -25,7 +25,7 @@ graph * create_graph(int nb_nodes, bool is_directed)
 {
     graph * g = (graph *) malloc(sizeof(graph));
     g->nb_nodes = nb_nodes;
-    g->ng_edges = 0;
+    g->nb_edges = 0;
     g->is_directed = is_directed;
     g->edges = (edge_node * *) malloc(sizeof(edge_node *) * nb_nodes);
 
@@ -61,18 +61,20 @@ void graph_add_edge(graph * g, int u, int v, int weight)
     edge_node * e = (edge_node *) malloc(sizeof(edge_node));
     e->v = v;
     e->weight = weight;
-    e->next = g->edges[u];
+    // add at the first element in the edges to complete operation in O(1)
+    e->next = g->edges[u]; // point v to u. represent v -> u direction (edge)
     g->edges[u] = e;
 
     if(!g->is_directed) {
+        // u should point to v also, if it is undirected graph
         e = (edge_node *) malloc(sizeof(edge_node));
         e->v = u;
         e->weight = weight;
-        e->next = g->edges[v];
+        e->next = g->edges[v]; // point u to v. represent u -> v direction
         g->edges[v] = e;
     }
 
-    g->ng_edges++;
+    g->nb_edges++;
 }
 
 int main(int argc, char* argv[])
